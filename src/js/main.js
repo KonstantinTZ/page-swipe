1ba78629368f1d8ede9d335f45ccbb25c9b9b24b
Konstantin Tavaratsyan <147811423+KonstantinTZ@users.noreply.github.com>
2025-02-06 12:53:44 +0300

    add new library

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 import './_components.js';
+import {PageFlip} from 'page-flip';
+const singleFlip = new PageFlip(document.getElementById('single-book'),
+    {
+        width: 400, // required parameter - base page width
+        height: 600  // required parameter - base page height
+    }
+);
+
+singleFlip.loadFromHTML(document.querySelectorAll('.single-page'));
